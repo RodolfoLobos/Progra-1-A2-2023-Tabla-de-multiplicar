5efebd046005d8999b67bf2626a556b979c68383
RodolfoLobos <stevenlovoz3@gmail.com>
2023-08-28 22:49:10 -0600

    Tabla de multiplicar con suma

--- Tabla-de-multiplicar/Form1.Designer.cs
+++ Tabla-de-multiplicar/Form1.Designer.cs
@@ -32,6 +32,11 @@ namespace Tabla_de_multiplicar
             this.btnProcesar = new System.Windows.Forms.Button();
             this.lblTablaDeMultiplicar = new System.Windows.Forms.Label();
             this.txtTablaDeMultiplicar = new System.Windows.Forms.TextBox();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnProcesar
@@ -63,11 +68,63 @@ namespace Tabla_de_multiplicar
             this.txtTablaDeMultiplicar.Size = new System.Drawing.Size(185, 35);
             this.txtTablaDeMultiplicar.TabIndex = 2;
             // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(535, 68);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(90, 27);
+            this.lblNum1.TabIndex = 3;
+            this.lblNum1.Text = "Num 1:";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(809, 68);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(90, 27);
+            this.lblNum2.TabIndex = 4;
+            this.lblNum2.Text = "Num 2:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(631, 65);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 35);
+            this.txtNum1.TabIndex = 5;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(905, 60);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 35);
+            this.txtNum2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(722, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Sumar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(693, 450);
+            this.ClientSize = new System.Drawing.Size(1126, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
             this.Controls.Add(this.txtTablaDeMultiplicar);
             this.Controls.Add(this.lblTablaDeMultiplicar);
             this.Controls.Add(this.btnProcesar);
@@ -84,6 +141,11 @@ namespace Tabla_de_multiplicar
         private System.Windows.Forms.Button btnProcesar;
         private System.Windows.Forms.Label lblTablaDeMultiplicar;
         private System.Windows.Forms.TextBox txtTablaDeMultiplicar;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
